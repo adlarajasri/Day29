323be78cc9bccd64b0566efde3b6d94a1c81083a
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-30 10:22:33 +0530

    Add files via upload

---
+++ FileReadOrWrite/FileReadOrWrite/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReadOrWrite
+{
+    internal class ExcelMapper
+    {
+        static void Main(string[] args)
+        {
+            List<Student> str = new List<Student>()
+
+        {
+
+            new Student{Id=101,Name="Alok"},
+
+            new Student{Id=102,Name="Ramesh"},
+
+            new Student{Id=103,Name="Shubha"}
+
+        };
+
+            ExcelMapper mapper = new ExcelMapper();
+
+            var newfile = @"d:\Student.xlsx";
+
+            mapper.Save(newfile, str, "StudData", true);
+
+            List<Student> students = mapper.Fetch<Student>("StudData").ToList();
+
+            Console.WriteLine("StudData List");
+
+            foreach (Student student in students)
+
+            {
+
+                Console.WriteLine(student.Id + "\t" + student.Name);
+
+            }
+
+            Console.WriteLine();
+
+        }
+
+    }
+
+    public class Student
+
+    {
+
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+    
+}
